e9847077abead1d78e906ae1899a2f0d0eed6f1d
Luke <luke.wallin@gmail.com>
2014-07-19 13:27:09 +0100

    pointing at head of master in LukesLibrary

---
+++ LensFlareStar.js
@@ -0,0 +1,17 @@
+/* 
+ * To generate a lens flare star
+ */
+
+var LensFlareStar=function(){
+    
+    /**
+     * 
+     * @param {type} canvas to draw on
+     * @param {type} size of star
+     * @param {type} pos Vector, position relative to top left of canvas to draw
+     * @returns {undefined}
+     */
+    this.draw=function(canvas,size,pos){
+        
+    }
+}
---
+++ NebularGenerator.js
@@ -0,0 +1,24 @@
+/* 
+ * Attempt at generating pictures of nebuli.  Hopefully will be useful for planet wars 3
+ */
+
+
+/**
+ * 
+ * @returns {undefined}
+ */
+var NebularGenerator=function(){
+    /* constructor */
+    
+    
+    /**
+     * Assuming it wants to be drawn the full size of the canvas
+     * @param {type} canvas - canvas on which to draw 
+     * @returns {undefined}
+     */
+    this.draw=function(canvas){
+        
+    }
+}
+
+
